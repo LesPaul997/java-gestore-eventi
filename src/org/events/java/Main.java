c35448d822ca6c176612bdf0e5dd91995244d02b
Mario Giordano <164010608+LesPaul997@users.noreply.github.com>
2024-07-03 21:13:06 +0200

    feat class Main

--- src/org/events/java/Main.java
+++ src/org/events/java/Main.java
@@ -11,6 +11,42 @@ public class Main {
 		Scanner scanner = new Scanner(System.in);
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-	}
-
+		String title;
+		LocalDate date = null;
+		int seats = 0;
+		
+		//Ciclo Do-While per l'inserimento del titolo
+		do {
+			System.out.print("Inserisci il titolo dell'evento: ");
+			title = scanner.nextLine();
+				if (title.isEmpty()) {
+					System.out.println("Errore: il titolo non può essere vuoto.");
+				}
+		} while (title.isEmpty());
+		
+		//Ciclo Do-while per l'inserimento della data
+		boolean valideDate = false;
+		do {
+            System.out.print("Inserisci la data dell'evento (dd/MM/yyyy): ");
+            String dataString = scanner.nextLine();
+            try {
+                date = LocalDate.parse(dataString, formatter);
+                if (date.isBefore(LocalDate.now())) {
+                    System.out.println("Errore: La data dell'evento non può essere nel passato.");
+                } else {
+                    valideDate = true;
+                }
+            } catch (DateTimeParseException e) {
+                System.out.println("Formato data non valido.");
+            }
+        } while (!valideDate);
+				
+				
+				
+				
+				
+		}
+		
 }
+
+
