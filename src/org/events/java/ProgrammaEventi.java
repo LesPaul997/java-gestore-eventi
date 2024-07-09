461987886bafb2dc2f59268d8bf0d135197b4994
Mario Giordano <164010608+LesPaul997@users.noreply.github.com>
2024-07-09 08:16:07 +0200

    add constructor and methods

--- src/org/events/java/ProgrammaEventi.java
+++ src/org/events/java/ProgrammaEventi.java
@@ -16,6 +16,22 @@ public class ProgrammaEventi {
 		this.eventi = new ArrayList<>();
 	}
 	
+	//Metodo che aggiunge un evento alla lista
+	public void addEvento(Evento evento) {
+		this.eventi.add(evento);
+	}
+	
+	//Metodo che restituisce una lista con tutti gli eventi presenti in una certa data
+	public List<Evento> getEventiByDate(LocalDate date) {
+		List<Evento> eventiByDate = new ArrayList<>();
+			for (Evento evento : this.eventi) {
+				if (evento.getDate().equals(date)) {
+					eventiByDate.add(evento);
+				}
+			}
+			return eventiByDate;
+	}
+	
 	
 	
 }
